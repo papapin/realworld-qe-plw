dd3ba1a8da32a462a1703c5fc920b1e63c2a1535
qafromla <diana.la777@address.com>
2023-07-06 12:41:58 -0700

    create a new test article & create article-helper

---
+++ tests/login/article.spec.ts
@@ -0,0 +1,20 @@
+import { test, expect, type Page } from '@playwright/test'
+import { USER_NAME, PASSWORD, EMAIL } from '../../environment.config'
+import { login } from '../../src/helpers/ui/login-helpers'
+import { article } from '../../src/helpers/ui/article-helpers'
+
+test.beforeEach(async ({ page }) => {
+	await page.goto('/')
+})
+
+test.describe('Article', () => {
+	const newArticle = 'article' + Date.now()
+	const aboutArticle = 'article' + Date.now()
+	const writeArticle = 'some text'
+	const tags = 'article'
+	test('should be able to create an article with Valid Inputs @QALA-25', async ({ page }) => {
+		await login(page, EMAIL, PASSWORD)
+		await article(page, newArticle, aboutArticle, writeArticle, tags)
+		await expect(page.getByRole('heading', { name: newArticle })).toBeVisible()
+	})
+})
