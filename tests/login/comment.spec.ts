5080d794c8c718c3c2d9f305ce8f1054523c00dd
qafromla <diana.la777@address.com>
2023-07-06 13:09:35 -0700

    add new helper + test qase QALA-33

---
+++ tests/login/comment.spec.ts
@@ -0,0 +1,24 @@
+import { test, expect, type Page } from '@playwright/test'
+import { USER_NAME, PASSWORD, EMAIL } from '../../environment.config'
+import { login } from '../../src/helpers/ui/login-helpers'
+import { article } from '../../src/helpers/ui/article-helpers'
+import { comment } from '../../src/helpers/ui/comment-helper'
+
+test.beforeEach(async ({ page }) => {
+	await page.goto('/')
+})
+
+test.describe('Article', () => {
+	const newArticle = 'article' + Date.now()
+	const aboutArticle = 'article' + Date.now()
+	const writeArticle = 'some text'
+	const tags = 'article'
+	const commentText = 'nice article'
+	test('should be able to write and post comment under article @QALA-33', async ({ page }) => {
+		await login(page, EMAIL, PASSWORD)
+		await article(page, newArticle, aboutArticle, writeArticle, tags)
+		await page.getByRole('link', { name: newArticle }).click()
+		await comment(page, commentText)
+		await expect(page.getByRole('paragraph').filter({ hasText: commentText })).toBeVisible()
+	})
+})
